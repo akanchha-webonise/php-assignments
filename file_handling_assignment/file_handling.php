afc6ef2318f27da5bd07903b084cafe5823b6d7b
akanchhabhadauria-webonise <akanchha.bhadauria@weboniselab.com>
2017-12-13 19:18:05 +0530

    Updated Assignment

--- file_handling_assignment/file_handling.php
+++ file_handling_assignment/file_handling.php
@@ -11,6 +11,59 @@
 	if(isset($_POST['append_a_file'])){
 		appendAFile();
 	}
+	if(isset($_POST['upload_read_button'])){
+		uploadAndRead();
+	}
+	if(isset($_POST['upload_write_button'])){
+		uploadAndWrite();
+	}
+	if(isset($_POST['upload_append_button'])){
+		uploadAndRead();
+	}
+	function uploadAndRead(){
+		$filename=$_FILES['upload']['name'];
+		$tmp=$_FILES['upload']['tmp_name'];
+		$dst="/home/webonise/php/{$_FILES['upload']['name']}";
+		if(move_uploaded_file($tmp,$dst)){
+			$myfile=fopen($filename,"r");
+			echo fread($myfile,filesize("$filename"));
+			fclose($myfile);
+		}
+		else{
+			$error=$_FILES['upload']['error'];
+			echo $error;
+		}
+	}
+	function uploadAndWrite(){
+		$filename=$_FILES['upload']['name'];
+		$tmp=$_FILES['upload']['tmp_name'];
+		$dst="/home/webonise/php/{$_FILES['upload']['name']}";
+		if(move_uploaded_file($tmp,$dst)){
+			$myfile=fopen($filename,"w");
+			$txt=$_POST["data_input"];
+			fwrite($myfile,$txt);
+			fclose($myfile);
+		}
+		else{
+			$error=$_FILES['upload']['error'];
+			echo $error;
+		}
+	}
+	function uploadAndAppend(){
+		$filename=$_FILES['upload']['name'];
+		$tmp=$_FILES['upload']['tmp_name'];
+		$dst="/home/webonise/php/{$_FILES['upload']['name']}";
+		if(move_uploaded_file($tmp,$dst)){
+			$myfile=fopen($filename,"a");
+			$txt=$_POST["data_input"];
+			fwrite($myfile,$txt);
+			fclose($myfile);
+		}
+		else{
+			$error=$_FILES['upload']['error'];
+			echo $error;
+		}
+	}
 	function appendAFile(){
 		$name=$_POST["input_file_name"];
 		if(!file_exists("$name.txt")){  //Error Handling
@@ -21,8 +74,7 @@
 			$txt=$_POST["data_input"];
 			fwrite($myfile,"\n".$txt);
 			fclose($myfile);
-		}
-		
+		}	
 	}
 	function readAFile(){
 		$name=$_POST["input_file_name"];
@@ -59,7 +111,7 @@
 	<title>File Handling Assignment</title>
 </head>
 <body>
-	<form method="POST">
+	<form method="POST" enctype="multipart/form-data">
 		<label>Enter file name</label>
 		<input type="text" name="input_file_name"><br>
 		<label>Enter data to write</label>
@@ -68,6 +120,11 @@
 		<input type="submit" name="read_a_file" value="Read a file">
 		<input type="submit" name="write_a_file" value="Write a file">
 		<input type="submit" name="append_a_file" value="Append a file">
+		<br><br><br>
+		<input type="file" name="upload">
+		<input type="submit" name="upload_read_button" value="Upload and Read">
+		<input type="submit" name="upload_write_button" value="Upload and Write">
+		<input type="submit" name="upload_append_button" value="Upload and Append">
 	</form>
 </body>
 </html>
